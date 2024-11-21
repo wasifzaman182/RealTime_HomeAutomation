b6f003116c981ad621aed7aa474970c41c17ade3
adesh03 <36736177+adesh03@users.noreply.github.com>
2024-11-21 15:02:57 +0100

    test

---
+++ Codes/BlinkCode/BlinkCode.ino
@@ -0,0 +1,21 @@
+void setup() 
+{
+  pinMode(16, OUTPUT);     // Initialize the 16 pin as an output
+  pinMode(4, OUTPUT);     // Initialize the 16 pin as an output
+}
+
+// the loop function runs over and over again forever
+void loop() 
+{
+  digitalWrite(16, HIGH);   // Turn the LED on by making the voltage LOW
+  delay(6000);                      // Wait
+  digitalWrite(16, LOW);  // Turn the LED off by making the voltage HIGH
+  delay(2000);                      // Wait
+
+  digitalWrite(4, LOW);   // Turn the LED on by making the voltage LOW
+  delay(1000);                      // Wait
+  digitalWrite(4, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(3000);                      // Wait
+
+  // Wait
+}
